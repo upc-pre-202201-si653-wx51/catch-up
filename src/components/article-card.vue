9e56c00fe2aac24fcfa9bfd59c473d5fc12fec3f
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-19 14:11:52 -0500

    refactor: Extracted single article presentation into a separated component.

---
+++ src/components/article-card.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="ma-2 elevation-1" hover>
+    <v-img class="white--text align-end" cover
+           :src="article.urlToImage"
+           :alt="article.title"
+           gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)">
+      <v-card-title class="ma-3 text-white" v-text="article.title"></v-card-title>
+    </v-img>
+    <v-card-text class="ma-2">
+      {{ article.description }}
+    </v-card-text>
+    <v-card-actions class="ma-2">
+      <v-chip v-if="article.source.url" link :href="article.source.url" target="_blank" size="x-small" color="primary">
+        {{ article.source.name }}
+      </v-chip>
+      <v-chip v-else size="x-small" color="primary">
+        {{ article.source.name }}
+      </v-chip>
+      <v-spacer></v-spacer>
+      <v-btn icon="mdi-reddit" class="text-red"/>
+      <v-btn icon="mdi-twitter" class="text-blue-grey-lighten-1"/>
+      <v-btn icon="mdi-facebook" class="text-blue--darken-4"/>
+      <v-btn icon="mdi-google-plus" class="text-red"/>
+      <v-btn icon="mdi-linkedin" class="text-blue--darken-4"/>
+      <v-spacer></v-spacer>
+      <v-btn small replace color="info" :href="article.url" target="_blank">Read More</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "article-card",
+  props: {
+    article: null,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/main-content.vue
+++ src/components/main-content.vue
@@ -1,46 +1,14 @@
 <template>
   <div v-for="article in articles" :key="article.title">
-    <v-card class="my-3" hover>
-      <v-img height="350px" v-bind:src="article.urlToImage"></v-img>
-      <v-container fill-height fluid>
-        <v-layout>
-          <v-flex xs12 align-end d-flex>
-            <span class="headline">{{ article.title }}</span>
-          </v-flex>
-        </v-layout>
-      </v-container>
-      <v-card-text>
-        {{ article.description }}
-      </v-card-text>
-      <v-card-actions>
-        <v-chip class="ma-2" size="small" color="secondary" text-color="white">
-          {{ article.source.name }}
-        </v-chip>
-        <v-spacer></v-spacer>
-        <v-btn icon="mdi-reddit" color="red">
-        </v-btn>
-        <v-btn icon class="light-blue--text">
-          <v-icon small>mdi-twitter</v-icon>
-        </v-btn>
-        <v-btn icon class="blue--text text--darken-4">
-          <v-icon small>mdi-facebook</v-icon>
-        </v-btn>
-        <v-btn icon class="red--text">
-          <v-icon small>mdi-google-plus</v-icon>
-        </v-btn>
-        <v-btn icon class="blue--text text--darken-4">
-          <v-icon small>mdi-linkedin</v-icon>
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn small replace color="info" v-bind:href="article.url" target="_blank">Read More</v-btn>
-      </v-card-actions>
-    </v-card>
+    <article-card :article="article"/>
   </div>
 </template>
 
 <script>
+import ArticleCard from "./article-card.vue";
 export default {
   name: "MainContent",
+  components: {ArticleCard},
   props: {
     articles: Array
   }
